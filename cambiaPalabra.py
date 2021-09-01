af2c947bbafbabacc7d8121f2ccd40f2974321bd
Iberikos <Ruben89pm@gamil.com>
2021-09-01 09:48:42 +0200

    Version semifinal

--- cambiaPalabra.py
+++ cambiaPalabra.py
@@ -2,6 +2,16 @@ import sys
 
 argumentos = sys.argv
 
+if len(argumentos) < 2:
+    f = input("Nombre de ficheros: ")
+    argumentos.append(f)
+
+if len(argumentos) < 3:
+    argumentos.append(input("palabra original:"))
+    
+if len(argumentos) < 4:
+    argumentos.append(input("nueva palabra: "))
+
 nombreF = argumentos[1]
 original = argumentos[2]
 nueva = argumentos[3]
